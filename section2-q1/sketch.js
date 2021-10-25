b004d67fa5f30a171c4443f03711c7f0139b9008
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-10-25 10:48:06 +0900

    change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100);
+  fill(0);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  nofill();
+  ellipse(50,50,i*10);
+  if(i<5){
+    stroke(255,0,0)
+  }
+  else{
+    stroke(0,0,255)
+  }
+      // BLANK[1]
   }
 }
