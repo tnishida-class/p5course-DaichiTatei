f7cfb43c3d7ee07af3c3f976f65fe1e745a4fe60
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-10-31 20:29:58 +0900

    Change Section3-1q

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,27 @@
-// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
+// 「心臓の鼓動のように大きくなったり小さくなったりする円(クリックで移動可)」
 let count;
-let cycle;
+let x,y,v;
 
 function setup(){
   createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
+  count = 100;
+  x=width/2
+  y=height/2
+  v=5;
+
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  count+=v;
+
+  if(count>150||count<50){v=-1*v}
+  ellipse(x, y, count);
+
+  count = constrain(count, 0, width);
+}
+function mouseClicked(){
+  x=mouseX
+  y=mouseY
+
 }
