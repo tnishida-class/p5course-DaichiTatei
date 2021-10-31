582e54f92a36dafdfa20b819371527c9ff0e6b10
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-10-31 16:28:32 +0900

    Change Section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,15 +2,15 @@
 function setup(){
   createCanvas(100,100);
   fill(0);
-  for(let i = 0; i < 11; i++){
-  ellipse(50,50,i*10);
-　noFill();
+  noFill();
+  for(let i = 0; i < 10; i++){
   if(i<5){
     stroke(255,0,0)
   }
   else{
     stroke(0,0,255)
   }
+  ellipse(50,50,(i+1)*10)
       // BLANK[1]
   }
 }
