401df058ca6b9f21654895ab7301dcce83c9b1e4
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-11-09 10:48:50 +0900

    Changes section4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -6,7 +6,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
   console.log(scores);
 
@@ -19,32 +19,48 @@ function setup(){
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
+  average = sum/scores.length;
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
+    if(largest<scores[i]){
+      largest=scores[i];
+    }
     // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
   }
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
+    if(smallest>scores[i]){
+      smallest=scores[i];
+    }
     // BLANK[3]　ヒント：最小値とだいたい同じ
   }
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
+
   noStroke();
 
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
+    if(scores[i]==largest){
+       fill(255,0,0);
+    }
+    else if(scores[i]==smallest){
+       fill(0,0,255);
+    }
+    else{fill(77)};
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
     rect(i * dx + 2, height - h, dx - 4, h);
     fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
+  stroke(0,175,3);
+  line(0,400-height*average/100,width,400-height*average/100);
 
   // BLANK[5] 平均点の線を引きます
 }
