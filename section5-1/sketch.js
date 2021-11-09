5b98a7c3b2e29847afc5aecb671bfd9e1248f891
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-11-09 14:39:36 +0900

    Change section5-1

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -1,14 +1,24 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
-  background(192);
+  createCanvas(300, 200);
+  background(0,51,153);
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    let x = 100 + cos(theta) * 65;
+    let y = 100 + sin(theta) * 65;
+    noStroke()
+    fill(255,204,0)
+    star(x, y, 12);
   }
 }
-
-// ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
+function star(cx, cy, r){
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 5; i++){
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    const x = 50 + cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
+}
