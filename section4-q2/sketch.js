a6963cb45309a5a5718f1d7afa6b029ad8f9d49e
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-11-22 13:47:28 +0900

    change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,12 +7,20 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
-
-  // BLANK[1]
+  for(let i = 0; i < scores.length; i++){
+    total += scores[i];
+  }
+  for(let j = 0; j < scores.length; j++){
+    const cx = width / 2;
+    const cy = height / 2;
+    const R = width * 0.4;
+    let point = 0;
+    point += scores[j];
+    arc(cx,cy,R,R,TWO_PI*(point-scores[j])/total,TWO_PI*point/total,PIE);
+  }
 }
