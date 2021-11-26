10f5a4ef0afcd1e83d39b9cc084026630f800738
Daichi Tatei <1931021h@stu.kobe-u.ac.jp>
2021-11-26 22:57:22 +0900

    Change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -15,12 +15,13 @@ function setup(){
   for(let i = 0; i < scores.length; i++){
     total += scores[i];
   }
-  for(let j = 0; j < scores.length; j++){
-    const cx = width / 2;
-    const cy = height / 2;
-    const R = width * 0.4;
-    let point = 0;
-    point += scores[j];
-    arc(cx,cy,R,R,TWO_PI*(point-scores[j])/total,TWO_PI*point/total,PIE);
+  let start=0
+  for(let i = 0; i < scores.length; i++){
+    const cx = width /2;
+    const cy = height /2;
+    const R = width * 0.6;
+    let end = scores[i]/total+start;
+    arc(cx,cy,R,R,TWO_PI*start,TWO_PI*end,PIE);
+    start = end;
   }
 }
